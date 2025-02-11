73fab18f3f7163ff60e85e8d9dce80d0016c273f
Ben Grav <bngrav@gmail.com>
2025-02-11 22:27:18 +1300

    Tested using Tiled for tilemap

---
+++ tiled/parallelogram_test.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="parallelogram_test" tilewidth="19" tileheight="12" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="pg1.png" width="19" height="12"/>
+ </tile>
+ <tile id="1">
+  <image source="pg2.png" width="19" height="12"/>
+ </tile>
+</tileset>
