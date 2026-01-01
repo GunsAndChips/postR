8a893d6b4f8ac9bd4a9be5d30d3b5d8fe4b96a3b
Ben Grav <bngrav@gmail.com>
2026-01-01 15:05:10 +1300

    Added check for circular reference to Clone function for cloning a table to prevent stackoverflow error due to infinite loop

--- helpers.lua
+++ helpers.lua
@@ -1,12 +1,28 @@
-function Clone(table)
+function Clone(_table, allTablesSeen, _depth)
+    allTablesSeen = allTablesSeen or {}
+    local _depth = _depth or 0
+    print("Running Clone function, depth: " .. _depth)
+
     local copy = {}
-    for key, value in pairs(table) do
+    for key, value in pairs(_table) do
         if type(value) == "table" then
-            copy[key] = Clone(value)
+            print("Found table property: " .. tostring(key))
+            if #allTablesSeen > 0 then
+                for i = 1, #allTablesSeen do
+                    if _table == allTablesSeen[i] then
+                        error("unable to clone table '" .. tostring(_table) .. "', due to circular reference.")
+                    end
+                end
+            end
+
+            table.insert(allTablesSeen, _table)
+            copy[key] = Clone(value, allTablesSeen, _depth + 1)
+            table.remove(allTablesSeen)
         else
             copy[key] = value
         end
     end
+    print("Exiting clone function...")
     return copy
 end
 
