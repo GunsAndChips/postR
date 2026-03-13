e16546292500128aea9314a7b32de3a55baaee6d
Ben Grav <bngrav@gmail.com>
2026-03-14 12:20:49 +1300

    Renamed Clone function to CloneTable and commented out as I'm no longer using it

--- helpers.lua
+++ helpers.lua
@@ -1,32 +1,32 @@
 require "log"
 
-function Clone(_table, allTablesSeen, depth)
-    allTablesSeen = allTablesSeen or {}
-    depth = depth or 0
-    log.debug("Entering Clone function, depth: " .. depth)
+-- function CloneTable(_table, allTablesSeen, depth)
+--     allTablesSeen = allTablesSeen or {}
+--     depth = depth or 0
+--     log.debug("Entering Clone function, depth: " .. depth)
 
-    local copy = {}
-    for key, value in pairs(_table) do
-        if type(value) == "table" then
-            log.debug("Found table property: " .. tostring(key))
-            if #allTablesSeen > 0 then
-                for i = 1, #allTablesSeen do
-                    if _table == allTablesSeen[i] then
-                        error("unable to clone table '" .. tostring(_table) .. "', due to circular reference.")
-                    end
-                end
-            end
+--     local copy = {}
+--     for key, value in pairs(_table) do
+--         if type(value) == "table" then
+--             log.debug("Found table property: " .. tostring(key))
+--             if #allTablesSeen > 0 then
+--                 for i = 1, #allTablesSeen do
+--                     if _table == allTablesSeen[i] then
+--                         error("unable to clone table '" .. tostring(_table) .. "', due to circular reference.")
+--                     end
+--                 end
+--             end
 
-            table.insert(allTablesSeen, _table)
-            copy[key] = Clone(value, allTablesSeen, depth + 1)
-            table.remove(allTablesSeen)
-        else
-            copy[key] = value
-        end
-    end
-    log.debug("Exiting Clone function...")
-    return copy
-end
+--             table.insert(allTablesSeen, _table)
+--             copy[key] = CloneTable(value, allTablesSeen, depth + 1)
+--             table.remove(allTablesSeen)
+--         else
+--             copy[key] = value
+--         end
+--     end
+--     log.debug("Exiting Clone function...")
+--     return copy
+-- end
 
 function OverwriteTable(table, overwriteWith)
     local newTable = {}
